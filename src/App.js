d7535e26286fabf415e819c8de399b1888431668
nammahari <hariprasanth581@gmail.com>
2023-09-15 22:29:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <br></br><br></br>
+        <div className='body_here'style={{textAlign: "center"}}>
+          <h2>Inspire. Create. Transform.</h2>
+          <h1>we are on a mission to transform your ideas into powerful digital solutions.</h1>
+          <p>Our goal is simple - to help your business succeed by offering comprehensive solutions tailored to your unique needs.</p>
+        </div>
+        <div className='connect'>
+            <a className='connect_button' href='https://tally.so/r/n99AjY' target='_blank'>
+              Connect
+            </a>
+        </div>
+      
     </div>
   );
+  
 }
-
 export default App;
